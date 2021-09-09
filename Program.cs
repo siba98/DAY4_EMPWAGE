e89555356efe50617d280bb94af79bf415b2ab64
siba98 <gyansiba.patro@gmail.com>
2021-09-09 21:33:35 +0530

    Siba: added the program.cs in which the class files are compiling

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //UC1_EmployeeAttendance.GetAttendance();
+            //We have to create object if static keyword is not used like:
+            //UC1_EmployeeAttendance obj = new UC1_EmployeeAttendance();
+            //obj.GetAttendance();
+            //UC2_CalculatingEmpWage.CalEmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            //UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("HCL",30,22,176);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
