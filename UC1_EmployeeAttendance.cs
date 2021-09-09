78ac01cc95320aed8d0f6bf6bf28ca3dc916d269
siba98 <gyansiba.patro@gmail.com>
2021-09-09 15:27:46 +0530

    Siba: added employee attendance

---
+++ UC1_EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    class UC1_EmployeeAttendance
+    {
+        public static void GetAttendance()
+        {
+            //Constants                   
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+        }
+
+    }
+}
