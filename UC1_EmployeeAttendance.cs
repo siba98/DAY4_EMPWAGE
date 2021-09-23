4fd1ea212aaedc80990eb3420cf1996b6f9b028b
siba98 <gyansiba.patro@gmail.com>
2021-09-23 19:54:41 +0530

    Siba: Modified employee attendance

--- UC1_EmployeeAttendance.cs
+++ UC1_EmployeeAttendance.cs
@@ -14,9 +14,9 @@ namespace Day4
             //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is present");
             else
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is absent");
             Console.ReadKey();
         }
 
